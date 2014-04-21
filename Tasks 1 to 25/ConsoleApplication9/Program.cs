da98f942202728692f477c4184adc132c22852f9
slop3n <slop3n@gmail.com>
2014-04-21 18:39:59 +0300

    update

---
+++ Tasks 1 to 25/ConsoleApplication9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 1000; a++)
+            {
+                for (int b = 1; b < 1000; b++)
+                {
+                    for (int c = 1; c < 1000; c++)
+                    {
+                        if (a + b + c == 1000 && Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2) && a < b && b < c)
+                        {
+                            Console.WriteLine(a * b * c);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
